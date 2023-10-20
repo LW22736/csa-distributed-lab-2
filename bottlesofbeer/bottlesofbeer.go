fa385f637df4caac59d784cb112f99b1988b36f4
Thomas Parr <github.us8r1@tpalias.com>
2023-10-20 14:17:00 +0100

    Finishes bottles of beer

--- bottlesofbeer/bottlesofbeer.go
+++ bottlesofbeer/bottlesofbeer.go
@@ -1,20 +1,70 @@
 package main
+
 import (
 	"flag"
-//	"net/rpc"
-//	"fmt"
-//	"time"
-//	"net"
+	"fmt"
+	"net"
+	"net/rpc"
+	//	"net/rpc"
+	//	"fmt"
+	//	"time"
+	//	"net"
 )
 
 var nextAddr string
 
+var SingVerseHandler = "BuddyOperations.SingVerse"
 
-func main(){
-//	thisPort := flag.String("this", "8030", "Port for this process to listen on")
+type Response struct {
+	Message bool
+}
+
+type Request struct {
+	Message int
+}
+
+type BuddyOperations struct{}
+
+func (b *BuddyOperations) SingVerse(req Request, res *Response) (err error) {
+	doVerse(req.Message)
+
+	res.Message = true
+	return
+}
+
+func doVerse(n int) {
+	if n > 0 {
+		fmt.Println(n, "bottles of beer on the wall,", n, "bottles of beer. Take one down, pass it around...")
+		res := new(Response)
+		client, _ := rpc.Dial("tcp", nextAddr)
+		client.Go(SingVerseHandler, Request{n - 1}, res, nil)
+	} else {
+		fmt.Println("The crippling alcoholism has hit...")
+	}
+}
+
+func main() {
+	thisPort := flag.String("this", "8030", "Port for this process to listen on")
 	flag.StringVar(&nextAddr, "next", "localhost:8040", "IP:Port string for next member of the round.")
-//	bottles := flag.Int("n",0, "Bottles of Beer (launches song if not 0)")
+	bottles := flag.Int("n", 0, "Bottles of Beer (launches song if not 0)")
 	flag.Parse()
-	//TODO: Up to you from here! Remember, you'll need to both listen for
-	//RPC calls and make your own.
+
+	err := rpc.Register(&BuddyOperations{})
+	if err != nil {
+		panic(err)
+	}
+
+	listener, _ := net.Listen("tcp", ":"+*thisPort)
+	defer func(listener net.Listener) {
+		err := listener.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(listener)
+
+	if *bottles != 0 {
+		go doVerse(*bottles)
+	}
+	fmt.Println("Starting to run rpc")
+	rpc.Accept(listener)
 }
